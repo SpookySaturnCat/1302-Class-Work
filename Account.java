056a5a866cb1b9175a1ea53fe84373d2ed7b344c
Seraphina <124868030+SpookySaturnCat@users.noreply.github.com>
2024-01-16 23:38:41 -0500

    Add files via upload

--- Account.java
+++ Account.java
@@ -1,19 +1,20 @@
-import java.sql.Date;
+import java.util.Date;
 
 public class Account {
 	// Private data feilds
 	private int id;
 	private double balance, annualIntrestRate;
 	private Date dateCreated;
-	
+
 	// Default no-args constructor
 	public Account() {
 		// Mutate data members
 		setId(0);
 		setBalance(0.00);
 		setAnnualIntrestRate(0.0);
+		dateCreated = new Date();
 	}
-	
+
 	// Creates account with id and balance
 	public Account(int id, double balance) {
 		// Create instance and mutate
@@ -22,6 +23,15 @@ public class Account {
 		setBalance(balance);
 	}
 
+	public void withdraw(double amount) {
+		setBalance(getBalance() - amount);
+	}
+	
+	public void deposit(double amount) {
+		setBalance(getBalance() + amount);
+	} 
+	
+	// Acsessors and mutators 
 	public int getId() {
 		return id;
 	}
@@ -42,9 +52,15 @@ public class Account {
 		return annualIntrestRate;
 	}
 
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
 	public void setAnnualIntrestRate(double annualIntrestRate) {
 		this.annualIntrestRate = annualIntrestRate;
 	}
 
-	
+	public double getMonthlyIntrestRate() {
+		return getAnnualIntrestRate() / 12.0;
+	}
 }
