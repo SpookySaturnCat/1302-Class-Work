5d981ee169c1f331fe07aebc883312dcc3a11b1e
Seraphina <124868030+SpookySaturnCat@users.noreply.github.com>
2024-03-02 23:41:30 -0500

    Add files via upload

---
+++ coffeeshop/CoffeeShop.java
@@ -0,0 +1,16 @@
+/*
+ *  Actual game
+ */
+package coffeeshop;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CoffeeShop extends Game {
+	
+
+}
